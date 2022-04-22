a0dddcebaa00cf29bba06bf62ecc5eb1b69a457c
rkdmf7220 <rkdmf7220@gmail.com>
2022-04-22 21:08:53 +0900

    기본 페이지(Home, Works, About, Contact) 생성

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+  .home{
+    img{
+      width: 100%;
+    }
+  }
+</style>
