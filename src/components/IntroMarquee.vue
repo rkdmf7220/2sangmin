df15c70b12501d436e7c3002b4990f298c159418
rkdmf7220 <rkdmf7220@gmail.com>
2022-04-22 21:11:36 +0900

    marquee 이펙트 용 컴포넌트 생성

---
+++ src/components/IntroMarquee.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="marquee-list">
+    <li class="marquee-item">
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "IntroMarquee"
+}
+</script>
+
+<style scoped lang="scss">
+  .marquee-list{
+    width: 100%;
+    height: 100%;
+
+    .marquee-item{
+      height: 20%;
+    }
+  }
+</style>
