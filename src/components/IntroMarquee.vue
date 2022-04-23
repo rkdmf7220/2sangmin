7b25f3c7a4b8fd3b29e0d5be7cc56bb46d6eb9a8
rkdmf7220 <rkdmf7220@gmail.com>
2022-04-23 12:32:27 +0900

    intro 이미지 추가 및 marquee 백그라운드 이미지 적용

--- src/components/IntroMarquee.vue
+++ src/components/IntroMarquee.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="marquee-list">
-    <li class="marquee-item">
+    <li class="marquee-item" v-for="(n, index) in 5" :key="index" v-bind:style="{backgroundImage: 'url(./img/marquee/img_intro_' + n + '.png)'}">
     </li>
   </ul>
 </template>
@@ -15,9 +15,11 @@ export default {
   .marquee-list{
     width: 100%;
     height: 100%;
+    position: absolute;
 
     .marquee-item{
       height: 20%;
+      background-repeat: repeat-x;
     }
   }
 </style>
