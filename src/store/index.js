ec8bc58c191d1134a88e737444cb766c9e33a23d
rkdmf7220 <rkdmf7220@gmail.com>
2022-04-23 12:33:05 +0900

    homeInfo 엑시오스 데이터 연결

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,30 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import axios from "axios";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        homeInfo: {}
+    },
+    mutations: {
+        homeInfo(state, info) {
+            state.homeInfo = info
+        }
+    },
+    actions: {
+        loadHomeInfo({commit}) {
+            return axios.get("../data/intro.json").then(response => {
+                if(response.statusText === "OK") {
+                    commit('homeInfo', response.data)
+                } else {
+                    console.log("문제 발생")
+                    // TODO: error handling
+                }
+            }).catch(error => {
+                console.log('error', error)
+                // TODO: error handling
+            })
+
+        }
+    },
+    modules: {}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,49 @@
 <template>
-  <div class="home">
+  <div class="intro">
+    <intro-marquee/>
+    <div class="sticky-box">
+      <span>{{homeInfo.subTitle}}</span>
+      <span>{{homeInfo.title}}</span>
+    </div>
+  </div>
+  <div class="about">
+
   </div>
 </template>
 
 <script>
 
+import IntroMarquee from "../components/IntroMarquee";
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {IntroMarquee},
+  computed: {
+    homeInfo() {
+      return this.$store.state.homeInfo
+    }
+  },
+  mounted() {
+    this.$store.dispatch('loadHomeInfo')
+  }
 }
 </script>
 
 <style lang="scss">
-  .home{
-    img{
-      width: 100%;
+  .intro{
+    height: 225vh;
+
+    .sticky-box{
+      width: 600px;
+      height: 200px;
+      position: sticky;
+      top: 33vh;
+      margin: 0 auto 0 auto;
+      border: 1px solid #ddd6cb;
+      color: white;
+      background-color: #181212;
     }
+  }
+  .about{
+    height: 2000px;
   }
 </style>
